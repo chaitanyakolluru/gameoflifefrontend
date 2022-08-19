c64cc9cc413b99980244349af05e15b67b310479
Chaitanya Kolluru <chaitanya.kolluru@aa.com>
2022-08-18 19:43:30 -0700

    gridding works

---
+++ src/MyGrid.tsx
@@ -0,0 +1,90 @@
+import { default as Grid } from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import { Box } from '@mui/system';
+import * as React from 'react';
+import { Cell } from './backend/Cell';
+
+const ItemDark = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#1A2027',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const ItemLight = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#ffff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const CellLight: React.FC<{ size: number }> = ({ size }) => {
+  return (
+    <Grid xs={size}>
+      <ItemLight>item</ItemLight>
+    </Grid>
+  );
+};
+
+const CellDark: React.FC<{ size: number }> = ({ size }): JSX.Element => {
+  return (
+    <Grid xs={size}>
+      <ItemDark>item</ItemDark>
+    </Grid>
+  );
+};
+
+const Row: React.FC<{ size: number; cells: Cell[] }> = ({
+  size,
+  cells,
+}): JSX.Element => {
+  const cellsArray = cells.map((c: { alive: boolean; x: number; y: number }) =>
+    c.alive ? <CellLight size={size} /> : <CellDark size={size} />
+  );
+  return <>{cellsArray}</>;
+};
+
+const MyGrid: React.FC<{ size: number; cells: Cell[] }> = ({ size, cells }) => {
+  return (
+    <Box
+      sx={{
+        width: 218,
+        height: 180,
+        backgroundColor: 'primary.dark',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
+        flexGlow: 1,
+      }}
+    >
+      <Grid container spacing={0}>
+        <Row
+          size={size}
+          cells={[cells[0], cells[1], cells[1], cells[1], cells[1]]}
+        />
+        <Row
+          size={size}
+          cells={[cells[0], cells[1], cells[2], cells[3], cells[2]]}
+        />
+        <Row
+          size={size}
+          cells={[cells[0], cells[1], cells[2], cells[3], cells[2]]}
+        />
+        <Row
+          size={size}
+          cells={[cells[0], cells[1], cells[2], cells[3], cells[2]]}
+        />
+        <Row
+          size={size}
+          cells={[cells[0], cells[1], cells[2], cells[3], cells[2]]}
+        />
+      </Grid>
+    </Box>
+  );
+};
+
+export default MyGrid;
--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,27 @@ import { createRoot } from 'react-dom/client';
 import { awesomeThingsHappen } from './backend';
 import { Cell } from './backend/Cell';
 import { gridGenerator } from './backend/gridGenerator';
+import MyGrid from './MyGrid';
 
-const App: React.FC<{ c: Cell[] }> = ({ c }) => {
-  const ListItem: React.FC<{ c: Cell }> = ({ c }) => {
-    return <li>{c.toString()}</li>;
-  };
+const App: React.FC<{ cells: Cell[] }> = ({ cells }) => {
+  // const ListItem: React.FC<{ c: Cell }> = ({ c }) => {
+  //   return <li>{c.toString()}</li>;
+  // };
 
-  const list = c.map((cell) => <ListItem key={cell.toString()} c={cell} />);
+  // const cells = c.map((cell) => <ListItem key={cell.toString()} c={cell} />);
 
   return (
     <div>
       <h1>Game of Life:</h1>
-      <ol>{list}</ol>
+      {/* <ol>{cells}</ol> */}
+      <MyGrid size={0} cells={cells} />
     </div>
   );
 };
 
 const [gr, n] = gridGenerator(5);
 createRoot(document.getElementById('root')!).render(
-  <App c={awesomeThingsHappen(gr, n)} />
+  <App cells={awesomeThingsHappen(gr, n)} />
 );
 
 console.log(awesomeThingsHappen(gr, n));
